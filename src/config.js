08d51fd4c346d6fc1a086aab01a926fc285468c8
JC Coles <jcoles1155@gmail.com>
2023-12-12 23:49:25 -0800

    swaping port to 5432 works now when connecting to postico

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ export const db = {
   user: process.env.DB_USER || "postgres",
   password: process.env.DB_PASSWORD || "password",
   host: process.env.DB_HOST || "localhost",
-  port: process.env.DB_PORT || 5555,
-  database: process.env.DB_DATABASE || "tasksdb",
+  port: 5432,
+  database: process.env.DB_DATABASE || "template",
 };
 
 export const port = process.env.PORT || 4000;
